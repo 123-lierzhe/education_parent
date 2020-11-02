5d61b4a95375489b5994bb0bc7095f938add9fd1
liez <lierzhezzz@163.com>
2020-11-02 11:28:18 +0800

    添加提示信息

--- edu_course/edu_course_service/src/main/java/com/sjzc/course/controller/EduCourseController.java
+++ edu_course/edu_course_service/src/main/java/com/sjzc/course/controller/EduCourseController.java
@@ -3,6 +3,7 @@ package com.sjzc.course.controller;
 import com.sjzc.course.entity.vo.CourseAndDescribeVo;
 import com.sjzc.course.service.EduCourseService;
 import com.sjzc.utils.R;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +21,12 @@ public class EduCourseController {
 
     @PostMapping("insertCourseAndDescription")
     public R insertCourseAndDescription(@RequestBody CourseAndDescribeVo courseAndDescribeVo){
+        if(StringUtils.isBlank(courseAndDescribeVo.getTitle()) || StringUtils.isBlank(courseAndDescribeVo.getDescription()) ||
+                StringUtils.isBlank(courseAndDescribeVo.getTeacherId()) || StringUtils.isBlank(courseAndDescribeVo.getSubjectId()) ||
+                StringUtils.isBlank(courseAndDescribeVo.getSubjectParentId()) || courseAndDescribeVo.getLessonNum()==null ||
+                courseAndDescribeVo.getPrice()==null){
+            return R.error().message("请填写所有选项");
+        }
         try {
             String courseId = courseService.insertCourseAndDescription(courseAndDescribeVo);
             return R.oK().data("courseId",courseId);
